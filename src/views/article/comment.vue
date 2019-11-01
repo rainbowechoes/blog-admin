855b8ea862bfe38562caf0d464336e5d15548947
jer8ome <1762960779@qq.com>
2019-11-01 19:27:53 +0800

    local git init

---
+++ src/views/article/comment.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'comment'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/article/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <el-card class="box-card">
+    <div slot="header" class="clearfix">
+      <el-row :gutter="24">
+        <el-col :xs="10" :md="4"><span style="font-size: 30px">文章管理</span></el-col>
+        <el-col :xs="12" :md="20">
+          <el-button-group style="float: right; ">
+            <el-button type="primary" icon="el-icon-edit" size="small"></el-button>
+            <el-button type="primary" icon="el-icon-share" size="small"></el-button>
+            <el-button type="primary" icon="el-icon-delete" size="small"></el-button>
+          </el-button-group>
+        </el-col>
+      </el-row>
+    </div>
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+    </el-table>
+    <div class="block" style="margin-top: 5px">
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage4"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+      </el-pagination>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
+
+  data() {
+    return {
+      currentPage4: 4,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    margin: 1%;
+    width: 98%;
+  }
+</style>
